05c836337fb89734eef826b13fcdfa7e14d0b87d
Victor Rodrigues <vr94006300@gmail.com>
2026-04-19 23:00:20 -0300

    Atualizar o ChipCategoria.js

--- js/componentes/ChipCategoria.js
+++ js/componentes/ChipCategoria.js
@@ -1,1 +1,27 @@
-.
+// Componente de Chip de Categoria
+(function(global) {
+    "use strict";
+
+    function criarChipCategoria(categoria, isAtivo, onClick) {
+        const chip = document.createElement('span');
+        chip.className = `categoria-chip ${isAtivo ? 'ativo' : ''}`;
+        chip.innerHTML = `<i data-lucide="${categoria.icone}"></i> ${categoria.nome}`;
+        chip.addEventListener('click', onClick);
+        return chip;
+    }
+
+    function renderizarChipsCategorias(container, categorias, categoriaSelecionada, onSelect) {
+        container.innerHTML = '';
+        categorias.forEach(cat => {
+            const chip = criarChipCategoria(cat, categoriaSelecionada === cat.nome, () => onSelect(cat.nome));
+            container.appendChild(chip);
+        });
+        lucide.createIcons();
+    }
+
+    global.ChipCategoria = {
+        criarChipCategoria,
+        renderizarChipsCategorias
+    };
+
+})(window);
