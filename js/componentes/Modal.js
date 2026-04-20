6646ba081eaf9e72802b06040f3823a0669cccb1
Victor Rodrigues <vr94006300@gmail.com>
2026-04-19 22:58:25 -0300

    Atualizar o Modal.js

--- js/componentes/Modal.js
+++ js/componentes/Modal.js
@@ -1,1 +1,20 @@
-.
+// Componente Modal (Base)
+(function(global) {
+    "use strict";
+
+    function abrirModal(modalOverlay) {
+        modalOverlay.style.display = 'flex';
+        setTimeout(() => modalOverlay.classList.add('ativo'), 10);
+    }
+
+    function fecharModal(modalOverlay) {
+        modalOverlay.classList.remove('ativo');
+        setTimeout(() => modalOverlay.style.display = 'none', 300);
+    }
+
+    global.Modal = {
+        abrirModal,
+        fecharModal
+    };
+
+})(window);
